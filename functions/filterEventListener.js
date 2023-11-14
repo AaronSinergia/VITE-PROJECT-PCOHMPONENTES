c6c952fe456dcdc535b6255f488401d5b4a89a84
Aaron <aaronromeromusic@gmail.com>
2023-11-14 22:42:41 +0100

    readme

--- functions/filterEventListener.js
+++ functions/filterEventListener.js
@@ -1,11 +1,15 @@
 import { selectedProductBucle } from './productsBucleAndCreate';
 
 export function filterEventListener(element) {
-  element.addEventListener('change', () => {
+  element.addEventListener('change', (ev) => {
     const divIMGRemove = document.querySelectorAll('.div_product');
     divIMGRemove.forEach((div) => {
       div.remove();
     });
+
+    if (!ev.target.checked) {
+      location.reload();
+    }
 
     selectedProductBucle();
   });
